acd5058a1fa8b4df4867bf97b73d90ca46e2480d
Adrien RICHARD <richardadrien0@gmail.com>
2020-10-15 23:17:35 +0200

    controller scene v1

---
+++ Assets/Scripts/ControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ControllerTest : MonoBehaviour
+{
+    public Material IdleMat;
+    public Material ActivatedMat;
+    public ControllerManager CM;
+
+    public GameObject triggerObj;
+
+    private void Update()
+    {
+        triggerObj.GetComponent<Renderer>().material = IdleMat;
+        if (CM.trigger)
+        {
+            triggerObj.GetComponent<Renderer>().material = ActivatedMat;
+            Debug.Log("pressend");
+        }
+    }
+}
